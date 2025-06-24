6c025d704e5e98adfdc4e6fabf2b23c2a154f76e
Michelle Gerwald <149069955+iloveproggen@users.noreply.github.com>
2025-06-24 17:13:29 +0200

    Enhance header animation and visual effects
    Replaced the 'flicker' animation with a new 'skew' animation for the header, added ::before and ::after pseudo-elements for layered visual effects, and introduced a detailed 'flicker' keyframes animation. Minor text update in typewriter.js and whitespace adjustments in index.html.

--- skripte/typewriter.js
+++ skripte/typewriter.js
@@ -1,7 +1,7 @@
 
     // set up text to print, each item in array is new line
     var aText = new Array(
-      "Welcome "
+      "Welcome... "
     );
     var iSpeed = 200; // time delay of print out
     var iIndex = 0; // start printing array at this posision
